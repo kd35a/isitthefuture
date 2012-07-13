0ba43e48c7872873bd755810dbcf5421eabbabd4
Fredrik Strandin <fredrik@strandin.name>
2012-07-13 08:03:39 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8" />
+		<title>Is it the future yet?</title>
+		<style type="text/css">
+			body {
+				background-color: white;
+				color: black;
+				text-align: center;
+			}
+			div#content {
+				margin-top: 50px;
+				font-family: sans-serif;
+				font-size: 200%;
+			}
+		</style>
+	</head>
+	<body>
+		<div id="content">
+			<img src="logo.png" width="512" height="210" alt="Is it the future?" />
+			<p>
+				<?php
+				$future = mktime(0, 0, 0, 10, 21, 2015);
+				$today = mktime(0, 0, 0);
+				if ($future == $today) {
+					echo "Yes, it is!";
+				} else {
+					$datetimeFuture = new DateTime(date("Y-m-d", $future));
+					$datetimeToday = new DateTime(date("Y-m-d", $today));
+					$interval = $datetimeToday->diff($datetimeFuture);
+					echo "Nope, you still have ".$interval->format("%a")." days to become a scientist/engineer and invent the stuff you are missing.\n";
+				}
+				?>
+			</p>
+		</div>
+	</body>
+</html>
